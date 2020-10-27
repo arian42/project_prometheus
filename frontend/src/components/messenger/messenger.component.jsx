7b6f0f845d74519b40d0ba1ea7400c5e1dd4dbd2
Pastour97 <parsa.pmk.code@gmail.com>
2020-10-27 20:34:10 +0330

    it shows messages you send locally now, but we should connect it to the backend

--- frontend/src/components/messenger/messenger.component.jsx
+++ frontend/src/components/messenger/messenger.component.jsx
@@ -3,30 +3,49 @@ import "./messenger.styles.scss";
 
 const Messenger = () => {
     
-    const[messages] = useState([
-            [
-                <span>yolo_24</span>,
-                <span>12:23</span>,
-                <span>hey! check this example layout that I created.</span>
-            ],
+    const[messages, setMessages] = useState([
+        [
+            <span>yolo_24</span>,
+            <span>12:23</span>,
+            <span>hey! check this example layout that I created.</span>
+        ],
+        [
+            <span>mark</span>,
+            <span>12:25</span>,
+            <span>This css sucks. You seriously need to work on your css skill</span>
+        ],
+        [
+            <span>yolo_24</span>,
+            <span>12:25</span>,
+            <span>&#128542;&#128542;&#128542;<br/> well that hurt</span>
+        ],
+        [
+            <span>mark</span>,
+            <span>12:26</span>,
+            <span>Don't forget to salt your pasta before boiling it</span>
+        ],
+    ]);
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = event => {
+        setMessage(event.target.value);
+    }
+
+    const sendMessage = async event => {
+        event.preventDefault();
+
+        setMessages([
+            ...messages,
             [
                 <span>mark</span>,
-                <span>12:25</span>,
-                <span>This css sucks. You seriously need to work on your css skill</span>
-            ],
-            [
-                <span>yolo_24</span>,
-                <span>12:25</span>,
-                <span>&#128542;&#128542;&#128542;<br/> well that hurt</span>
-            ],
-            [
-                <span>mark</span>,
-                <span>12:26</span>,
-                <span>Don't forget to salt your pasta before boiling it</span>
-            ],
+                <span>don't have any yet</span>,
+                <span>{message}</span>
+            ]
         ]);
-    
-    console.log(messages);
+
+        setMessage("");
+    }
 
     return(
         <div className="messenger">
@@ -41,13 +60,19 @@ const Messenger = () => {
                             <p className="time">{a[1]}</p>
                             <p className="txt">{a[2]}</p>
                         </div>
-                    ))}
+                    ))
+                }
             </div> 
 
-            <div className="send">
-                <input type="txt"></input>
+            <form className="send" onSubmit={sendMessage}>
+                <input
+                    type="txt"
+                    onChange={handleChange}
+                    value={message}
+                    placeholder="write your message"
+                ></input>
                 <button type="submit">send</button>
-            </div>
+            </form>
         </div>
     );
 };
