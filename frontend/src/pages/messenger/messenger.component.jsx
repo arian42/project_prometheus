4641560051dba6a1630b4cba386c53e199494fac
Pastour97 <parsa.pmk.code@gmail.com>
2020-12-09 01:02:24 +0330

    added a ConversationList component which can be a standalone page in mobile devices but I  havent implemented that yet, overall added a lot of components from a facebook messenger clone and added these packages: shave, axios + linked ionicons in the HTML

--- frontend/src/pages/messenger/messenger.component.jsx
+++ frontend/src/pages/messenger/messenger.component.jsx
@@ -1,10 +1,15 @@
 import Chat from '../../components/chat/chat.component';
 
+import ConversationList from '../../components/experimental/ConversationList/index';
+
 import './messenger.styles.scss';
 
 const Messenger = () => {
     return(
         <div className="App">
+            <div className="scrollable sidebar">
+                <ConversationList />
+            </div>
             <Chat/>
         </div>
     );
