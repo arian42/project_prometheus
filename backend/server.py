de89c7f729ac2941be4c432c00c9c2998ba8525d
Aryan <arian.heydari.42@gmail.com>
2020-12-11 22:54:55 +0330

    new API OOOOOOOOOOOoooooooo......

--- backend/server.py
+++ backend/server.py
@@ -18,46 +18,26 @@ CORS(app)
 app.config['SECRET_KEY'] = 'not-that-secret'
 app.config["DEBUG"] = True
 
-
 EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+FILE_REGEX = re.compile(r"[\w]{100}")
 
 
 class data:
     def __init__(self):
-        for file_name in ("users.pickle", "chat.pickle"):
-            if not Path(file_name).is_file():
-                with open(file_name, 'bw+') as f:
-                    if file_name == "users.pickle":
-                        pickle.dump(dict(), f)
-                    else:
-                        pickle.dump(list(), f)
+        if not Path('users.pickle').is_file():
+            with open("users.pickle", 'bw+') as f:
+                pickle.dump(dict(), f)
 
-        self.chat_records = self._read_data('chat.pickle')
-        self._chat_file = open('chat.pickle', 'ab+')
         self.users = pickle.load(open('users.pickle', 'rb'))
         self.users_by_name = self._users_by_name()
 
-    def _read_data(self, file_name):
-        recoreds = []
-        with open(file_name, 'rb') as f:
-            while True:
-                try:
-                    recoreds.append(pickle.load(f))
-                except EOFError:
-                    break
-        return recoreds
+    def _gen_filename(self, user_id, user2_id):
+        user_id, user2_id = int(user_id), int(user2_id)
+        if user_id > user2_id:
+            return "chat/" + str(user_id) + str(user2_id) + ".pickle"
+        else:
+            return "chat/" + str(user2_id) + str(user_id) + ".pickle"
 
-    def save_chat(self, message, userid):
-        chat_id = int(str(userid) + str(int(time.time())))
-        data = {
-            'id': chat_id,
-            'author': self.users[userid]["username"],
-            'message': message,
-            'timestamp': int(time.time() * 1000)
-        }
-        self.chat_records.append(data)
-        with open('chat.pickle', 'wb') as f:
-            pickle.dump(self.chat_records, f)
 
     def _users_by_name(self):
         """ this makes user queries eazy and fast. this will run at runtime """
@@ -66,6 +46,41 @@ class data:
             foo[user['username']] = uid
         return foo
 
+
+    def read_chat(self, user_id, user2_id):
+        chat_records = None
+        with open(self._gen_filename(user_id, user2_id), 'rb') as f:
+            chat_records = pickle.load(f)
+        return chat_records
+
+
+    def save_chat(self, message, user_id, user2_id):
+        chat_id = int(str(user_id) + str(int(time.time())))
+        data = {
+            'id': chat_id,
+            'author': self.users[user_id]["username"],
+            'message': message,
+            'timestamp': int(time.time() * 1000)
+        }
+
+        dd = {
+            "newmsg" : 0,
+            "time": data['timestamp'],
+            "lastmsg": data['message']
+        }
+        if self.users[user_id].get(user2_id):
+            dd["newmsg"] = self.users[user_id][user2_id]['newmsg'] + 1
+        self.users[user_id][user2_id] = dd
+
+        chat_records = None
+        with open(self._gen_filename(user_id, user2_id), 'rb') as f:
+            chat_records = pickle.load(f)
+        chat_records.append(data)
+        with open(self._gen_filename(user_id, user2_id), 'wb') as f:
+            pickle.dump(chat_records, f)
+        return chat_records
+
+
     def add_user(self, email, username, name, password):
         while True:
             # this is not safe random but it is ok for this use
@@ -73,7 +88,7 @@ class data:
             user_id = getrandbits(64)
             if not user_id in self.users:
                 self.users[user_id] = {
-                    "username": username, "email": email, "name": name, "password": password}
+                    "username": username, "email": email, "name": name, "password": password, "friends": [], 'cash':{}}
                 self.users_by_name[username] = user_id
                 break
 
@@ -83,12 +98,26 @@ class data:
         return user_id
 
 
+    def start_chat(self, user_id, user2_id):
+        user_id, user2_id = int(user_id), int(user2_id)
+        if not self.users[user_id].get('friends'): # if users dose not have frinds 
+            self.users[user_id]['friends'] = []
+
+        if user2_id not in self.users[user_id]['friends']: # we will add user now
+            # create chat file
+            with open(self._gen_filename(user_id, user2_id), 'bw+') as f:
+                pickle.dump(list(), f)
+            # update user profile
+            self.users[user_id]['friends'].append(user2_id)
+            self.users[user2_id]['friends'].append(user_id)
+            with open('users.pickle', 'wb') as f:
+                pickle.dump(self.users, f)
+
+            
 # db like instance
 db = data()
 
 # decorator for verifying the JWT
-
-
 def token_required(func):
     @wraps(func)
     def decorated(*args, **kwargs):
@@ -97,6 +126,8 @@ def token_required(func):
         # jwt is passed in the request header
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
+        elif request.args.get('token'):
+            token = request.args.get('token')
         else:
             # if it is not in the headers we try to read from json post data
             try:
@@ -144,6 +175,9 @@ def signup():
 
     if not EMAIL_REGEX.match(data['email'].lower()):
         return make_response(jsonify({'error': "Email is not valid."}), 400)
+    
+    # if not FILE_REGEX.match(data['username'].lower()):
+    #     return make_response(jsonify({'error': "usernaem must only contain [ a-z 0-9 and _ ]"}), 400)
 
     user_id = db.add_user(data['email'].lower(),
                           data['username'].lower(), data['name'], md5(data['password'].encode('utf-8')).hexdigest())
@@ -204,15 +238,72 @@ def profile(user_id, *argv, **kwargs):
     return make_response(jsonify(target), 200)
 
 
-@app.route('/api/chat', methods=['GET', 'POST'])
+@app.route('/api/search/<username>', methods=['GET'])
+@app.route('/api/search', methods=['POST', 'GET'])
 @token_required
-def chat(user_id, *argv, **kwargs):
+def search(user_id, username=None, *argv, **kwargs):
+    ans = []
     if request.method == 'POST':
         d = request.get_json(force=True)
-        db.save_chat(d.get('msg', "NO data sent."), user_id)
+        username = d.get('username').lower()
+    if request.method == 'GET' and not username:
+        username = request.args.get('username')   
+    print(username)
+    print(db.users)
+    if username and db.users:
+        for uid, ddd in db.users.items():
+            if username in ddd['username']:
+                obj = {
+                    'name': db.users[uid]['name'],
+                    'username': db.users[uid]['username'],
+                    'avatar': "#"
+                }
+                ans.append(obj)
+    return make_response(jsonify(ans), 200)
+
+
+@app.route('/api/chat/<username>', methods=['GET', 'POST'])
+@token_required
+def chat(user_id, username, *argv, **kwargs):
+    user2_id = db.users_by_name.get(username.lower())
+    if user2_id:
+        db.start_chat(user_id, user2_id)
+    else:
+        return make_response(jsonify({'error': "user not found."}), 400)
+    
+    if request.method == 'POST':
+        d = request.get_json(force=True)
+        db.save_chat(d.get('msg', "NO data sent."), user_id, user2_id)
         return jsonify(success=True)
     else:
-        return make_response(jsonify(db.chat_records), 200)
+        return make_response(jsonify(db.read_chat(user_id, user2_id)), 200)
+
+
+@app.route('/api/chats', methods=['GET', "POST"])
+@token_required
+def conversations(user_id, *argv, **kwargs):
+    res = []
+    print(db.users)
+    for ids in db.users[user_id].get('friends', []):
+        profile = {
+            'name': db.users[ids]['name'],
+            'username': db.users[ids]['username'],
+            'avatar': "#"
+        }
+        gg = db.users[ids].get(user_id, {
+            "newmsg" : 0,
+            "time": 0,
+            "lastmsg": None
+        })
+        res.append({
+            "profile": profile,
+            "newmsg" : gg['newmsg'],
+            "time": gg['time'],
+            "lastmsg": gg['lasmsg']
+        })
+
+    newlist = sorted(res, key=lambda k: k['time']) 
+    return make_response(jsonify(newlist), 200)
 
 
 app.run()
