c46b7ef9e2392937cd4793987ad96b9ca3f5ce30
Pastour97 <parsa.pmk.code@gmail.com>
2021-01-15 21:54:30 +0330

    searching people now works ^_^

--- frontend/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ frontend/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,12 +5,12 @@ import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
 function SignInAndSignUp() {
-    return (
-      <div className="sign-in-and-sign-up">
-        <SignIn/>
-        <SignUp/>
-      </div>
-    );
-  }
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn/>
+      <SignUp/>
+    </div>
+  );
+}
   
 export default SignInAndSignUp;
