d6a2b14a6112985ebe373a96f29e55dba156f36a
Aryan <arian.heydari.42@gmail.com>
2021-01-15 23:23:35 +0330

    chat list is working....

--- backend/test2.sh
+++ backend/test2.sh
@@ -1,20 +1,30 @@
-url='http://127.0.0.1:5000/api/sign-up'
-json='{"email":"aryan4@example.com", "password":"12345678", "name":"aryan", "username":"aryan4"}'
+#url='http://127.0.0.1:5000/api/sign-up'
+#json='{"email":"ali@example.com", "password":"12345678", "name":"ali", "username":"ali"}'
+#
+#url='http://127.0.0.1:5000/api/sign-in'
+#json='{"usernameOrEmail":"ali", "password":"12345678"}'
+#
+#url='http://127.0.0.1:5000/api/search'
+#json='{"username": "a"}'
+#
+#url='http://127.0.0.1:5000/api/profile'
+#
+#
+#url='http://127.0.0.1:5000/api/chat/aryan'
+#json='{"msg": "ME?"}'
+#
 
-url='http://127.0.0.1:5000/api/sign-in'
-json='{"usernameOrEmail":"aryan", "password":"12345678"}'
+#url='http://127.0.0.1:5000/api/chat/ali'
+#json='{"msg": "hellow"}'
 
-url='http://127.0.0.1:5000/api/search'
-json='{"username": "a"}'
+#url='http://localhost:5000/api/chats'
 
-url='http://127.0.0.1:5000/api/profile'
+#Aryan TOKEN>>>
+#token='x-access-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MjU2ODQwNTQ3MiwiZXhwIjoxNjEwODIyNjM3fQ.rSU7toRTbvPqco5lVZjOI2quJtsmSnsG_TuRaimMdgY'
 
+#ALI token>>>
+#token='x-access-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MzIzOTc1NzUwNSwiZXhwIjoxNjEwODI1NDQ1fQ.jS_WHZcb3XNd6KDD_Kxi4TRotG7TUBC9QbOsWkMK8xs'
 
-url='http://127.0.0.1:5000/api/chat/aryan3'
-json='{"msg": "hellow"}'
-
-token='x-access-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNjEwNTU3NDIxfQ.CKJENUcocPdvehHiEGjbhriDHb9Wf0Ycl5OMotbk-r8'
-
-curl -X POST -H "$token" -d "$json" $url
-curl -H "$token" $url
-
+#curl -X POST -H "$token" -d "$json" $url
+#curl -H "$token" $url
+#curl -d "$json" $url
