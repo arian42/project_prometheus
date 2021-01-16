32409d95591577e29035c63aae39a78a1aaeee76
Bahare Samini <bahare.samini@gmail.com>
2021-01-16 15:55:32 +0330

    login page header added. not responsive yet

--- frontend/src/components/header/header.component.jsx
+++ frontend/src/components/header/header.component.jsx
@@ -1,8 +1,27 @@
+import prometheus from '../../../src/resource/prometheus.png';
+import logo from '../../../src/resource/logo-flame.png';
 import './header.styles.scss';
 
 const Header = () => (
     <div>
-        <h1>Prometheus Massenger</h1>
+        <div className="top-navbar">
+            <a className="logo"  href="#">
+                <img src={logo} alt=""/>
+            </a>
+            <div className="link">
+                <a href="#">Github</a>
+            </div>
+        </div>
+        <div className="header-content">
+            <div className="title">
+                <h1>Prometheus<br></br>Messenger</h1>
+                <p>The hottest messenger you have ever seen</p>
+                <a className="cta" href="#">Start Messaging</a>
+            </div>
+            <div className="img">
+                <img src={prometheus} alt=""/>
+            </div>
+        </div>
     </div>
 )
 
